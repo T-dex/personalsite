ec946cf65fbdbf53e0ea3db15646eec009f562f4
T-Dex <dex.m.mills@gmail.com>
2017-07-20 02:22:19 -0600

    First Build

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="app-header">
+          <div className="Logo"><h1> Dex Mills Designs</h1></div> </div>
+        <p className="App-intro">
+
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
