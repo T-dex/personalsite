24f1aa08a5b2670dff864a81d515688e8c37cba7
T-Dex <dex.m.mills@gmail.com>
2017-07-20 04:01:19 -0600

    Add CSS and footer component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Footer from './components/footerLinks2/socialLinks.js';
 import './styles/App.css';
 
 class App extends Component {
@@ -6,10 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <div className="app-header">
-          <div className="Logo"><h1> Dex Mills Designs</h1></div> </div>
+          <div className="Logo"><h1> Dex Mills Designs</h1><p>test</p></div> </div>
         <p className="App-intro">
-
+          Lorum fucking ipsom
         </p>
+      <div className="app-footer"> <Footer/><p> The Dinosaur Engineering Expierence 2017</p></div>
       </div>
     );
   }
---
+++ src/components/footerLinks2/socialLinks.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Footer extends Component{
+  constructor(props){
+    super(props)
+}
+  render(){
+    return (
+      <div className="footer">
+      <h4><a href="https://twitter.com/dexmills">twitter</a></h4>
+      <h4><a href="https://medium.com/@dexmills">medium</a></h4>
+      <h4><a href="https://github.com/T-dex/">github</a></h4>
+      </div>
+    )
+  }
+}
+
+
+export default Footer
