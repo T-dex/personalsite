3c7c159e910cbc1977b07456135f423338dd3689
T-Dex <dex.m.mills@gmail.com>
2017-07-20 17:30:59 -0600

    Add basic components and CSS style

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import Footer from './components/footerLinks2/socialLinks.js';
 import './styles/App.css';
+import About from './components/about';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="app-header">
-          <div className="Logo"><h1> Dex Mills Designs</h1><p>test</p></div> </div>
-        <p className="App-intro">
-          Lorum fucking ipsom
-        </p>
-      <div className="app-footer"> <Footer/><p> The Dinosaur Engineering Expierence 2017</p></div>
+          <div className="Logo"><h1> Dex Mills Designs</h1></div>
+       </div>
+        <div className="App-intro">
+          <div>
+            <About />
+          </div>
+        </div>
+        <div className="app-footer">
+          <Footer/><p> The Dinosaur Engineering Expierence 2017</p>
+        </div>
       </div>
     );
   }
---
+++ src/components/about.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+
+class About extends Component{
+
+  render(){
+    return (
+      <div>
+        <div className="profile">
+            <div className="Hero">
+              <h2>Loud and Original</h2>
+            </div>
+            <div className="bio">
+              <div className="bioflex">
+                <h1>Adventure based web design and pursuing the next big idea.</h1>
+              </div>
+            </div>
+            <div className="Hero2">
+              <h2>Design thru Adventure</h2>
+            </div>
+            <div className="portfolio">
+
+            </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default About;
